0cba2dff5be5538aa6210fef8b56f37696d54837
iliass <iliass.marchoud@gmail.com>
2019-09-18 10:37:21 +0200

    start

--- HeavyRain.php
+++ HeavyRain.php
@@ -17,6 +17,16 @@ class HeavyRain extends CityBuilder
 
         /* add your code here */
 
+        for ($j = 0; $j < count($city); $j++) {
+            $waterSize = 0;
+            $left = max(array_slice($city, 0, $j));
+            $right = max(array_slice($city, $j));
+            echo $city[$j]."\n";
+            echo "left : ".$left."\n";
+            echo "right : ".$right."\n";
+            echo "end\n";
+        }
+
         echo json_encode($city) . " => " . $result . "\n";
     }
 }
